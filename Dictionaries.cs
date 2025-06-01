79c1ac8773e39ac948a63220fcd3bca34ebeffe6
unknown <PhantomDergwulf>
2025-06-01 03:38:40 +0100

    fixed Dictionaries, added some event names

--- Dictionaries.cs
+++ Dictionaries.cs
@@ -50,89 +50,135 @@ public class Dictionaries
 
 public static readonly Dictionary<string, string> ValuablePaths = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
 {
+//Tiny Generic
     {"Valuable Diamond", "valuables/01 tiny/Valuable Diamond"},
     {"Valuable Emerald Bracelet", "valuables/01 tiny/Valuable Emerald Bracelet"},
     {"Valuable Goblet", "valuables/01 tiny/Valuable Goblet"},
     {"Valuable Ocarina", "valuables/01 tiny/Valuable Ocarina"},
     {"Valuable Pocket Watch", "valuables/01 tiny/Valuable Pocket Watch"},
     {"Valuable Uranium Mug", "valuables/01 tiny/Valuable Uranium Mug"},
-    {"Valuable Arctic Bonsai", "valuables/02 small/Valuable Arctic Bonsai"},
-    {"Valuable Arctic HDD", "valuables/02 small/Valuable Arctic HDD"},
-    {"Valuable Chomp Book", "valuables/02 small/Valuable Chomp Book"},
+    //Tiny Museum
+    {"Valuable Cool Brain", "valuables/01 tiny/Valuable Cool Brain"},
+    {"Valuable Fish", "valuables/01 tiny/Valuable Fish"},
+    {"Valuable Golden Fish", "valuables/01 tiny/Valuable GoldFish"},
+    {"Valuable Tooth", "valuables/01 tiny/Valuable Tooth"},
+    {"Valuable Golden Tooth", "valuables/01 tiny/Valuable GoldTooth"},
+    {"Valuable Ruben Doll", "valuables/01 tiny/Valuable RubenDoll"},
+    {"Valuable Silver Fish", "valuables/01 tiny/Valuable SilverFish"},
+    {"Valuable Toast", "valuables/01 tiny/Valuable Toast"},
+    //Small Generic
     {"Valuable Crown", "valuables/02 small/Valuable Crown"},
     {"Valuable Doll", "valuables/02 small/Valuable Doll"},
     {"Valuable Frog", "valuables/02 small/Valuable Frog"},
     {"Valuable Gem Box", "valuables/02 small/Valuable Gem Box"},
     {"Valuable Globe", "valuables/02 small/Valuable Globe"},
-    {"Valuable Love Potion", "valuables/02 small/Valuable Love Potion"},
-    {"Valuable Money", "valuables/02 small/Valable Money"},
-    {"Valuable Music Box", "valuables/02 small/Valueable Music Box"},
-    {"Valuable Toy Monkey", "valuables/02 small/Vauble Toy Monkey"},
-    {"Valuable Uranium Plate", "valuables/02 small/Vauble Uranium Plate"},
+    {"Valuable Money", "valuables/02 small/Valuable Money"},
+    {"Valuable Toy Monkey", "valuables/02 small/Valuable Toy Monkey"},
+    {"Valuable Uranium Plate", "valuables/02 small/Valuable Uranium Plate"},
     {"Valuable Vase Small", "valuables/02 small/Valuable Vase Small"},
-    {"Valuable Arctic 3D Printer", "valuables/03 medium/Valuable Arctic 3D Printer"},
+    //Small Arctic
+    { "Valuable Arctic Bonsai", "valuables/02 small/Valuable Arctic Bonsai"},
+    {"Valuable Arctic HDD", "valuables/02 small/Valuable Arctic HDD"},
+    //Small Wizard
+    {"Valuable Chomp Book", "valuables/02 small/Valuable Chomp Book"},
+    {"Valuable Love Potion", "valuables/02 small/Valuable Love Potion"},
+    //Small Manor
+    {"Valuable Music Box", "valuables/02 small/Valuable Music Box"},
+    //Small Museum
+    //Medium Generic
+    {"Valuable Bottle", "valuables/03 medium/Valuable Bottle"},
+    {"Valuable Clown", "valuables/03 medium/Valuable Clown"},
+    {"Valuable Vase", "valuables/03 medium/Valuable Vase"},
+    {"Valuable Trophy", "valuables/03 medium/Valuable Trophy"},
+    //Medium Arctic
+    { "Valuable Arctic 3D Printer", "valuables/03 medium/Valuable Arctic 3D Printer"},
     {"Valuable Arctic Laptop", "valuables/03 medium/Valuable Arctic Laptop"},
     {"Valuable Arctic Propane Tank", "valuables/03 medium/Valuable Arctic Propane Tank"},
     {"Valuable Arctic Sample Six Pack", "valuables/03 medium/Valuable Arctic Sample Six Pack"},
-    {"Valuable Arctic Sample", "valuables/03 medium/Valable Bottle"},
-    {"Valuable Bottle", "valuables/03 medium/Valable Bottle"},
-    {"Valuable Clown", "valuables/03 medium/Valueable Clown"},
-    {"Valuable Computer", "valuables/03 medium/Vauble Computer"},
-    {"Valuable Fan", "valuables/03 medium/Vauble Fan"},
-    {"Valuable Gramophone", "valuables/03 medium/Vauble Gramophone"},
-    {"Valuable Marble Table", "valuables/03 medium/Vauble Marble Table"},
-    {"Valuable Radio", "valuables/03 medium/Vauble Radio"},
-    {"Valuable Ship In Bottle", "valuables/03 medium/Vauble Ship in a bottle"},
-    {"Valuable Trophy", "valuables/03 medium/Vauble Trophy"},
-    {"Valuable Vase", "valuables/03 medium/Vauble Vase"},
-    {"Valuable Wizard Goblin Head", "valuables/03 medium/Vauble Wizard Goblin Head"},
-    {"Valuable Wizard Power Crystal", "valuables/03 medium/Vauble Wizard Power Crystal"},
-    {"Valuable Wizard Time Glass", "valuables/03 medium/Vauble Wizard Time Glass"},
-    {"Valuable Arctic Barrel", "valuables/04 big/Valuable Arctic Barrel"},
+    {"Valuable Arctic Sample", "valuables/03 medium/Valuable Bottle"},
+    {"Valuable Computer", "valuables/03 medium/Valuable Computer"},
+    {"Valuable Fan", "valuables/03 medium/Valuable Fan"},
+    //Medium Wizard
+    {"Valuable Wizard Goblin Head", "valuables/03 medium/Valuable Wizard Goblin Head"},
+    {"Valuable Wizard Power Crystal", "valuables/03 medium/Valuable Wizard Power Crystal"},
+    {"Valuable Wizard Time Glass", "valuables/03 medium/Valuable Wizard Time Glass"},    
+    //Medium Manor
+    { "Valuable Gramophone", "valuables/03 medium/Valuable Gramophone"},
+    {"Valuable Radio", "valuables/03 medium/Valuable Radio"},    
+    {"Valuable Ship In Bottle", "valuables/03 medium/Valuable Ship in a bottle"},
+    //Medium Museum
+    //Big Generic
+    {"Valuable Vase Big", "valuables/04 big/Valuable Vase Big"},    
+    //Big Arctic
+    { "Valuable Arctic Barrel", "valuables/04 big/Valuable Arctic Barrel"},
     {"Valuable Arctic Big Sample", "valuables/04 big/Valuable Arctic Big Sample"},
     {"Valuable Arctic Creature Leg", "valuables/04 big/Valuable Arctic Creature Leg"},
     {"Valuable Arctic Flamethrower", "valuables/04 big/Valuable Arctic Flamethrower"},
     {"Valuable Arctic Guitar", "valuables/04 big/Valuable Arctic Guitar"},
     {"Valuable Arctic Sample Cooler", "valuables/04 big/Valuable Arctic Sample Cooler"},
-    {"Valuable Diamond Display", "valuables/04 big/Vauble Diamond Display"},
-    {"Valuable Ice Saw", "valuables/04 big/Vauble Ice Saw"},
-    {"Valuable Scream Doll", "valuables/04 big/Vauble Scream Doll"},
-    {"Valuable Television", "valuables/04 big/Vauble Television"},
-    {"Valuable Vase Big", "valuables/04 big/Vauble Vase Big"},
-    {"Valuable Wizard Cube Of Knowledge", "valuables/04 big/Vauble Wizard Cube of Knowledge"},
-    {"Valuable Wizard Master Potion", "valuables/04 big/Vauble Wizard Master Potion"},
-    {"Valuable Animal Crate", "valuables/05 wide/Vauble Animal Crate"},
-    {"Valuable Arctic Ice Block", "valuables/05 wide/Vauble Arctic Ice Block"},
-    {"Valuable Dinosaur", "valuables/05 wide/Vauble Dinosaur"},
-    {"Valuable Piano", "valuables/05 wide/Vauble Piano"},
-    {"Valuable Wizard Griffin Statue", "valuables/05 wide/Vauble Wizard Griffin Statue"},
-    {"Valuable Arctic Science Station", "valuables/06 tall/Vauble Arctic Science Station"},
-    {"Valuable Harp", "valuables/06 tall/Vauble Harp"},
-    {"Valuable Painting", "valuables/06 tall/Vauble Painting"},
-    {"Valuable Wizard Dumgolfs Staff", "valuables/06 tall/Vauble Wizard Dumgolfs Staff"},
-    {"Valuable Wizard Sword", "valuables/06 tall/Vauble Wizard Sword"},
-    {"Valuable Arctic Server Rack", "valuables/07 very tall/Vauble Arctic Server Rack"},
-    {"Valuable Golden Statue", "valuables/07 very tall/Valuable Golden Statue"},
+    {"Valuable Ice Saw", "valuables/04 big/Valuable Ice Saw"},
+    //Big Wizard
+    {"Valuable Wizard Cube Of Knowledge", "valuables/04 big/Valuable Wizard Cube of Knowledge"},
+    {"Valuable Wizard Master Potion", "valuables/04 big/Valuable Wizard Master Potion"},    
+    //Big Manor
+    {"Valuable Diamond Display", "valuables/04 big/Valuable Diamond Display"},
+    {"Valuable Scream Doll", "valuables/04 big/Valuable Scream Doll"},
+    {"Valuable Television", "valuables/04 big/Valuable Television"},    
+    //Big Museum
+    //Wide Generic
+    {"Valuable Animal Crate", "valuables/05 wide/Valuable Animal Crate"},    
+    //Wide Arctic
+    {"Valuable Arctic Ice Block", "valuables/05 wide/Valuable Arctic Ice Block"},       
+    //Wide Wizard
+    {"Valuable Wizard Griffin Statue", "valuables/05 wide/Valuable Wizard Griffin Statue"},
+    //Wide Manor
+    {"Valuable Dinosaur", "valuables/05 wide/Valuable Dinosaur"},
+    {"Valuable Piano", "valuables/05 wide/Valuable Piano"},
+    //Wide Museum
+    //Tall Generic
+    //Tall Arctic 
+    {"Valuable Arctic Science Station", "valuables/06 tall/Valuable Arctic Science Station"},    
+    //Tall Wizard
+    //Tall Manor
+    { "Valuable Painting", "valuables/06 tall/Valuable Painting"},
+    {"Valuable Harp", "valuables/06 tall/Valuable Harp"},   
+    //Tall Museum
+    //Very Tall Generic
+    //Very Tall Arctic
+    {"Valuable Arctic Server Rack", "valuables/07 very tall/Valuable Arctic Server Rack"},
+    //Very Tall Wizard
+    {"Valuable Wizard Broom", "valuables/07 very tall/Valuable Wizard Broom"},          
+    //Very Tall Manor
     {"Valuable Grandfather Clock", "valuables/07 very tall/Valuable Grandfather Clock"},
-    {"Valuable Wizard Broom", "valuables/07 very tall/Valuable Wizard Broom"}
+    {"Valuable Golden Statue", "valuables/07 very tall/Valuable Golden Statue"},         
+    //Very Tall Museum
+    {"Valuable Traffic light", "valuables/07 very tall/Valuable Traffic Light"},
+    //Unknown / Unimplimeneted
+    {"Valuable Marble Table", "valuables/03 medium/Valuable Marble Table"}
+ 
+
+
+
+
+
 };
 
 
     public static readonly Dictionary<string, string> EnemyPaths = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
         {
-            { "Beamer", "Beamer" },
+            { "Beamer", "Clown" },
             { "Duck", "Apex Predator" },
             { "Robe", "Robe" },
             { "Bowtie", "Bowtie" },
-            { "Floater", "Floater" },
-            { "Gnome", "Gnome" },
-            { "Hunter", "Hunter" },
+            { "Floater", "Mentalist" },
+            { "Gnome", "Gnomes" },
+            { "Hunter", "Huntsman" },
             { "Tumbler", "Chef" },
             { "Thin Man", "Shadow Child" },
             { "Slow Mouth", "Spewer" },
             { "Upscream", "Upscream" },
             { "Hidden", "Hidden" },
-            { "Bang", "Banger" },
+            { "Bang", "Bangers" },
             { "Head", "Headman" },
             { "Runner", "Reaper" },
             { "Slow Walker", "Trudge"}
